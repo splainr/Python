e70f6cb38a4db4ec248289751c2152d7d4c5a516
splainr <splainr@gmail.com>
2021-08-05 12:47:51 -0400

    Added Hello World scripts

---
+++ Python_Day2_with_Git.py
@@ -0,0 +1,22 @@
+#Prints the Words "Hello World"
+print ("Hello World")
+
+#This is a comment.
+
+'''
+This is a comment
+This is also a comment
+This is yet another comment
+'''
+
+print  ('Hello Python... Good Start :-).')
+
+# UserAge = 0
+
+UserAge, UserName = 30, 'Peter'
+print (UserAge)
+print (UserName)
+
+
+
+
