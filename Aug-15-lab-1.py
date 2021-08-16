4a36be9c3a2f0a5c2f17d15a9cf99e05fd6ce53e
splainr <splainr@gmail.com>
2021-08-15 23:26:34 -0400

    Create Aug 15 Lab

---
+++ Aug-15-lab-1.py
@@ -0,0 +1,8 @@
+print("----------------------------------------------------------")
+print("Date:        Aug 15 2021")
+print("Project:     Python learning lab")
+print("Exercise:    Learning Basic Operators")
+print("Author:      Russell J. Splain")
+print("Objective:   Learn Operators and Assignments")
+print("----------------------------------------------------------")
+print("")
