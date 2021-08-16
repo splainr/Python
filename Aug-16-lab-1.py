7615f3e0e3571c2769e91aa426c10d42cfc52b2b
splainr <splainr@gmail.com>
2021-08-16 08:25:52 -0400

    Aug-16-lab-start

---
+++ Aug-16-lab-1.py
@@ -0,0 +1,9 @@
+print("----------------------------------------------------------")
+print("Date:        Aug 16 2021")
+print("Project:     Python learning lab")
+print("Exercise:    Learning Basic Operators")
+print("Author:      Russell J. Splain")
+print("Objective:   Data Types in Python")
+print("----------------------------------------------------------")
+print("")
+#
