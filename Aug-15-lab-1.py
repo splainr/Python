050bb5b58ce6588086011a36ac973bf2ee3aff06
splainr <splainr@gmail.com>
2021-08-15 23:45:18 -0400

    Completed Lab Aug-15

--- Aug-15-lab-1.py
+++ Aug-15-lab-1.py
@@ -6,3 +6,20 @@ print("Author:      Russell J. Splain")
 print("Objective:   Learn Operators and Assignments")
 print("----------------------------------------------------------")
 print("")
+#
+# Assign value of 10 to x:
+x = 10
+# Assign value of (x + 2) to x:
+x = x + 2
+#
+print(x)
+#
+# Now combine the Assignment operator (=), with the Addition operator (+):
+x += 2
+print(x)
+#
+# Now combine the Assignment operator (=), with the Minus operator (-):
+x -= 2
+print(x)
+#
+# This work with all 7 Operators shown in Aug 8 lab.
