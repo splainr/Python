17aa400d27aaacc3f8310c3925d58289c53f0f66
splainr <splainr@gmail.com>
2021-08-16 09:01:51 -0400

    dang

--- Aug-16-lab-1.py
+++ Aug-16-lab-1.py
@@ -5,5 +5,12 @@ print("Exercise:    Chapter 4: Data Types in Python")
 print("Author:      Russell J. Splain")
 print("----------------------------------------------------------")
 print("")
+# VariableName = initial value
+# Integers:
+UserAge=20
+mobilenumber = 12398724
+print( UserAge , mobilenumber )
 #
-#
+# Float:
+UserHeight = 1.82, UserWeight = 67.2
+print( UserHeight , UserWeight )
