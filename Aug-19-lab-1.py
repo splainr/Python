b03bfa731b1fa5ee1f80ca1f012321ed9bab3a2d
splainr <splainr@gmail.com>
2021-08-19 23:41:00 -0400

    updates

--- Aug-19-lab-1.py
+++ Aug-19-lab-1.py
@@ -1,7 +1,7 @@
 print("----------------------------------------------------------")
-print("Date:        Aug 16 2021")
-print("Project:     Python Lab")
-print("Exercise:    Chapter 4: Data Types in Python")
+print("Date:        Aug 19 2021")
+print("Project:     Python  Lab")
+print("Exercise:    Chapter 4: Data Types in Python - continued.")
 print("Author:      Russell J. Splain")
 print("----------------------------------------------------------")
 print("")
@@ -10,7 +10,6 @@ print("")
 UserAge , CellNumber = 20 , 12398724
 # 
 print( "User Age:", UserAge , "Cell Number:", CellNumber )
-# print( "Cell Number:", CellNumber )
 #
 # Float:
 UserHeight = 1.82
