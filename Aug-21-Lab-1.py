b54021566d6aaf495ae1c376b216af04cce57bf5
splainr <splainr@gmail.com>
2021-08-21 17:48:24 -0400

    Python Lab work 8-21-21

--- Aug-21-Lab-1.py
+++ Aug-21-Lab-1.py
@@ -4,8 +4,8 @@
 # Exercise:    python.land Tutorial: Data Type: String")
 # Learner:     Russell J. Splain")
 # ----
-
 # print("")
+
 Num = 2 / 2 * 8
 # print(Num)
 
@@ -22,11 +22,37 @@ This is line 1,
 .this is line 2,
 ..this is line 3."""
 # print(MyBigString)
-# print("")
+
 
 # Here is an example of SINGLE (') and DOUBLE (") quotes escaped in a triple-quoted sentence: 
 Line = """He said: "Hello, I've got a question." as he raised hs hand."""
-print(Line)
-print("")
+# print(Line)
+
+# This exercise prints out a string variable in all lower, then all-upper case:
+String_3 = "Hello World!" 
+# print(String_3.lower())
+# print(String_3.upper())
+
+# This string operation produces '10', as the number of printable characters at which the letter 'd'
+# is found in "Hello World".
+# print(String_3.find('d'))
+
+# This string operation replaces character 'H' with 'J' and prints the ouput.
+# print(String_3.replace("H", "J"))
+
+# This string operation does these steps:
+# 1. prints variable String_3
+# 2. inserts a comment
+# 3. runs the len() function on variable String_3
+# 4. prints the string length. (12 characters in Hello World!, including the non-printing blank.)
+# print(String_3 , "-> This string Length is:" , len(String_3))
+
+
+
+
+
+
+
+
 
 
