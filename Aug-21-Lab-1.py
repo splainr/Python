6e9f8acb7b301724fb2199997458538fa673c93f
splainr <splainr@gmail.com>
2021-08-21 10:06:35 -0400

    Python Land Tutorial 8-21-21

---
+++ Aug-21-Lab-1.py
@@ -0,0 +1,32 @@
+# ----
+# Date:        Aug 21 2021")
+# Project:     Python Lab")
+# Exercise:    python.land Tutorial: Data Type: String")
+# Learner:     Russell J. Splain")
+# ----
+
+# print("")
+Num = 2 / 2 * 8
+# print(Num)
+
+String_1 = 'It\'s an escaped quote!'
+# print(String_1)
+# print("")
+
+String_2 = "I'm a so-called \"script kiddie\""
+# print(String_2)
+# print("")
+
+MyBigString = """
+This is line 1,
+.this is line 2,
+..this is line 3."""
+# print(MyBigString)
+# print("")
+
+# Here is an example of SINGLE (') and DOUBLE (") quotes escaped in a triple-quoted sentence: 
+Line = """He said: "Hello, I've got a question." as he raised hs hand."""
+print(Line)
+print("")
+
+
