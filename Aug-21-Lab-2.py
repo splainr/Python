722380ae304dc0853eebe8ad6ea74e21a50b3c52
splainr <splainr@gmail.com>
2021-08-22 08:42:26 -0400

    Updates on 8-21-21

---
+++ Aug-21-Lab-2.py
@@ -0,0 +1,19 @@
+# --------
+# Date:        Aug 21 2021")
+# Project:     Python Lab")
+# Exercise:    python.land Tutorial: Functions")
+# Learner:     Russell J. Splain")
+# ---------
+# print("")
+
+# print() is the most well-known, built-in function in Python.
+Num = 2 / 2 * 8
+# print(Num)
+
+# This example shows that print() displays output to the screen, but doesn't actually 'return' anything:
+result = print('Hello')
+print(result)
+
+mylength = len('Hello')
+print(mylength)
+
