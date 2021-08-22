ed2610062f4f26fa6139ca5c0b5493bc7d525164
splainr <splainr@gmail.com>
2021-08-22 11:53:52 -0400

    Rename file

---
+++ Aug-22-Lab-1.py
@@ -0,0 +1,17 @@
+# --------
+# Date:        Aug 22 2021")
+# Project:     Python Lab")
+# Exercise:    python.land Tutorial: Functions")
+# Learner:     Russell J. Splain")
+# ---------
+# print("")
+
+# Use the keyword 'def' to fine a function, like so:  def MyFunction():
+def say_hi():
+    print('Hi!')
+say_hi()
+
+
+
+
+
