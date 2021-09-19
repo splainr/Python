bdeeff38087c2c42109e80e0b35c30734681a850
splainr <splainr@gmail.com>
2021-09-19 13:25:22 -0400

    Sep-19-Lab-1

---
+++ Sep-19-Lab-1.py
@@ -0,0 +1,9 @@
+# --------
+# Date:        Sep 19 2021
+# Project:     Python Lab
+# Chapter:     Python FOR and WHILE loop
+# URL:         https://python.land/introduction-to-python/python-for-loop
+# Learner:     Russell J. Splain
+# ---------
+print("")
+
