677dc7c06d09416d642c307a4d6bfba92452b2bd
splainr <splainr@gmail.com>
2021-09-19 13:24:40 -0400

    Sep-13-Lab-1

---
+++ Sep-13-Lab-1.py
@@ -0,0 +1,35 @@
+# --------
+# Date:        Sep 13 2021
+# Project:     Python Lab
+# Chapter:     Python Boolean and Conditional Programming
+# URL:         https://python.land/introduction-to-python/python-boolean-and-operators
+# Exercise:    python.land Tutorial: What is a Boolean? Boolean Operators,
+# Learner:     Russell J. Splain
+# ---------
+print("")
+
+# door_is_locked = True
+# if door_is_locked:
+#     print("Mum, open the door!")
+
+# door_is_locked = False
+# if door_is_locked:
+#     print("Mum, open the door!")
+# else:
+#     print("Let's go inside")
+# print("")
+
+# Here are some Boolean Operators:
+print(2 > 1)
+2 < 1
+2 < 3 < 4 < 5 < 6
+2 < 3 > 2
+3 >= 3
+3 >= 2
+2 == 2
+4 != 5
+'a' == 'a'
+'a' == 'b'
+'a' > 'b'
+print('M' < 'm')
+print('1' < 'a')
