b876b8d218c13259277cd4a50c862a281e4f8bd5
splainr <splainr@gmail.com>
2022-01-05 09:02:10 -0500

    etc

---
+++ Jan-05_Lab-1_For-Loop.py
@@ -0,0 +1,26 @@
+# --------
+# Date:        Jan 05 2022
+# Project:     Python Lab
+# Chapter:     Python FOR and WHILE loop
+# URL:         https://python.land/introduction-to-python/python-for-loop
+# Learner:     Russell J. Splain
+# ---------
+print("")
+
+## Tested Script ##
+## for letter in ('Hello'):
+##    print(letter)
+
+# NOTE: The general template for a for-loop in Python is:
+
+# for <variable> in <iterable>:
+#   do something with variable
+
+# NOTE: Data type: Lists  (also called arrays in other languages)
+
+## Tested Script ##
+## mylist = [1, 'a', 'Hello']
+## for item in mylist:
+##    print(item)
+
+
